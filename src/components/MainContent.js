ab3e431a313aac6036195142fae36fa5e738df6c
William S <will.step.205@gmail.com>
2023-06-12 20:39:49 -0700

    background image added

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,7 +2,7 @@ export default function MainContent() {
     return (
         <div className="main-content--text">
             <h1>Reasons I'm excited to learn React</h1>
-            <ul>
+            <ul className="main--facts">
                 <li>It's a popular library, so I'll be 
                 able to fit in with the cool kids!</li>
                 <li>I'm more likely to get a job as a developer
