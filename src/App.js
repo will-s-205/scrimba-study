87182fab9b590f916f716a834795925b029ed90f
William S <will.step.205@gmail.com>
2023-06-12 19:25:04 -0700

    Navbar component updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Header from './components/Header'
+import Navbar from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
 
 export default function App() {
   return (
-    <div id="quote-box">
-      <Header />
+    <div id="container">
+      <Navbar />
       <MainContent />
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import react_logo from '../react-logo.png';
 
-export default function Header() {
+export default function Navbar() {
     return (
         <header>
             <nav className="nav-bar">
+                <div className="react-facts-and-icon">
                 <img src={react_logo} className="nav-logo" alt="react logo" />
-                <p style={{fontWeight: "bold"}}>ReactFacts</p>
+                <p className="react-facts">ReactFacts</p>
+                </div>
                 <ul className="nav-items">
                     <li>Pricing</li>
                     <li>About</li>
