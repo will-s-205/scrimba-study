87765d39aeab33366c8785937dd031f32775fe77
William S <will.step.205@gmail.com>
2023-06-16 11:48:19 -0700

    rendering HTML array of elements

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Navbar from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
 
+// export default function App() {
+//   return (
+//     <div className="container">
+//       <Navbar />
+//       <MainContent />
+//       <Footer />
+//     </div>
+//   );
+// }
+
 export default function App() {
+  // const colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+  // OR
+  const colors = [
+          <h3>Red</h3>, 
+          <h3>Orange</h3>, 
+          <h3>Yellow</h3>,
+          <h3>Green</h3>,
+          <h3>Blue</h3>,
+          <h3>Indigo</h3>,
+          <h3>Violet</h3>
+      ]
   return (
-    <div className="container">
-      <Navbar />
-      <MainContent />
-      <Footer />
-    </div>
-  );
+      <div>
+          {colors}
+      </div>
+  )
 }
