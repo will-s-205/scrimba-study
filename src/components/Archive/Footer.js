b3d6528f84f2339df570ca6cff01bf0540acce8e
William S <will.step.205@gmail.com>
2023-06-26 10:49:00 -0700

    files rearrangement

--- src/components/Archive/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='footer'>
-            <small>© 2023 William Step</small>
-            {/* <div class="footer">by <a href="https://codepen.io/hezag/">hezag</a></div> */}
-        </footer>
-    )
-}
