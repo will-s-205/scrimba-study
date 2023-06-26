95314981f8310b0f9831917e79ce96e953a2bbe2
William S <will.step.205@gmail.com>
2023-06-26 11:01:35 -0700

    Button rows added

--- src/components/Translator/AppTranslator.js
+++ src/components/Translator/AppTranslator.js
@@ -3,10 +3,32 @@ import './AppTranslator.scss';
 // https://scrimba.com/learn/learnreact/notes-app-sync-notes-with-localstorage-co3c5495b8d7949e81b79988a
 export default function App() {
 
-  return (
-    <div>
-      <h1>AppTranslator</h1>
+    function clicked() {
+        console.log("Clicked");
+    }
 
-    </div>
-  )
+    return (
+        <div>
+            <br></br>
+            <h1>AppTranslator</h1>
+            <br></br>
+            <div className="buttonRow">
+                <p>Translation row 1: </p>
+                <button onClick={clicked}>clicked 1</button>
+                <button onClick={clicked}>clicked 2</button>
+                <button onClick={clicked}>clicked 3</button>
+                <button onClick={clicked}>clicked 4</button>
+                <button onClick={clicked}>clicked 5</button>
+            </div>
+            <br></br>
+            <div className="buttonRow">
+                <p>Translation row 2: </p>
+                <button onClick={clicked}>clicked 1</button>
+                <button onClick={clicked}>clicked 2</button>
+                <button onClick={clicked}>clicked 3</button>
+                <button onClick={clicked}>clicked 4</button>
+                <button onClick={clicked}>clicked 5</button>
+            </div>
+        </div>
+    )
 }
