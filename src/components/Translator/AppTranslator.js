fc48ceeb5a9724d97abbbf98bb01ffdb13687589
William S <will.step.205@gmail.com>
2023-06-26 10:57:29 -0700

    Translator Project added

---
+++ src/components/Translator/AppTranslator.js
@@ -0,0 +1,12 @@
+import './AppTranslator.scss';
+
+// https://scrimba.com/learn/learnreact/notes-app-sync-notes-with-localstorage-co3c5495b8d7949e81b79988a
+export default function App() {
+
+  return (
+    <div>
+      <h1>AppTranslator</h1>
+
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/LocalStorage/AppLocalStorage';
+// import App from './components/LocalStorage/AppLocalStorage';
+import App from './components/Translator/AppTranslator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
