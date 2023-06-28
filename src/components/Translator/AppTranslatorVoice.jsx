66b0079ebb1af1842bb90fcd4e0c57ec40320b71
William S <will.step.205@gmail.com>
2023-06-28 13:48:51 -0700

    dropdown option language support

--- src/components/Translator/AppTranslatorVoice.jsx
+++ src/components/Translator/AppTranslatorVoice.jsx
@@ -1,14 +1,21 @@
+import { nanoid } from 'nanoid';
 import { useState, useEffect } from 'react';
 import './AppTranslatorVoice.scss';
 // import { useEffect, useState } from 'react';
 
 export default function AppTranslatorVoice() {
-    // const [options, setOptions] = useState[
-    //     {'data-lang': '', 'data-name': '' }
-    // ]
+    const [text, setText] = useState('')
+    const [voices, setVoices] = useState([]);
+    const [lang, setLang] = useState('');
+
+    console.log(text)
+    useEffect(() => {
+        // console.log("useEffect")
+        // localStorage.setItem("records", JSON.stringify(records))
+    }, [text])
 
     // // Init SpeechSynth API
-    // const synth = window.speechSynthesis;
+    const synth = window.speechSynthesis;
 
     // // DOM Elements
     // const textForm = document.querySelector('form');
@@ -64,52 +71,70 @@ export default function AppTranslatorVoice() {
     // //     }
     // // }
 
-    // // Speak
-    // const speak = () => {
-    //     // Check if speaking
-    //     if (synth.speaking) {
-    //         console.error('Already speaking...');
-    //         return;
-    //     }
-    //     if (textInput.value !== '') {
-    //         // Add background animation
-    //         // body.style.background = '#141414 url(img/wave.gif)';
-    //         // body.style.backgroundRepeat = 'repeat-x';
-    //         // body.style.backgroundSize = '100% 100%';
+    // Speak
+    const speak = () => {
+        // Check if speaking
+        if (synth.speaking) {
+            console.error('Already speaking...');
+            return;
+        }
+        if (text.value !== '') {
+            // Add background animation
+            // body.style.background = '#141414 url(img/wave.gif)';
+            // body.style.backgroundRepeat = 'repeat-x';
+            // body.style.backgroundSize = '100% 100%';
 
-    //         // Get speak text
-    //         const speakText = new SpeechSynthesisUtterance(textInput.value);
+            // Get speak text
+            const speakText = new SpeechSynthesisUtterance(text.text);
 
-    //         // Speak end
-    //         speakText.onend = e => {
-    //             console.log('Done speaking...');
-    //             body.style.background = '#141414';
-    //         };
+            // Speak end
+            speakText.onend = e => {
+                console.log('Done speaking...');
+                // body.style.background = '#141414';
+            };
 
-    //         // Speak error
-    //         speakText.onerror = e => {
-    //             console.error('Something went wrong');
-    //         };
+            // Speak error
+            speakText.onerror = e => {
+                console.error('Something went wrong');
+            };
 
-    //         // Selected voice
-    //         const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
-    //             'data-name'
-    //         );
+            // // Selected voice
+            // const selectedVoice = voices[0].getAttribute('name');
+            // WORKS!!!
+            // const selectedVoice = voices[9].name;
+            const selectedVoice = voices[9].name;
 
-    //         // Loop through voices
-    //         voices.forEach(voice => {
-    //             if (voice.name === selectedVoice) {
-    //                 speakText.voice = voice;
-    //             }
-    //         });
+            // function selectedVoiceFunction() {
+            //     let value
+            //     for(let i=0; i<voices.length; i++){
+            //         if(voices[i].default === true){
+            //             value = voices[i].name
+            //         }
+            //     }
+            //     // speakText.voice === value;
+            //     console.log(value)
+            // }
 
-    //         // Set pitch and rate
-    //         speakText.rate = rate.value;
-    //         speakText.pitch = pitch.value;
-    //         // Speak
-    //         synth.speak(speakText);
-    //     }
-    // };
+            // selectedVoiceFunction();
+
+            console.log(lang)
+
+            // const selectedVoice = voices[9].name
+
+            // // Loop through voices
+            voices.forEach(voice => {
+                if (voice.name === selectedVoice) {
+                    speakText.voice = voice;
+                }
+            });
+
+            // Set pitch and rate
+            // speakText.rate = rate.value;
+            // speakText.pitch = pitch.value;
+            // Speak
+            synth.speak(speakText);
+        }
+    };
 
     // EVENT LISTENERS
 
@@ -131,34 +156,10 @@ export default function AppTranslatorVoice() {
 
 
 
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////
 
-
-    // const dropDownVoices = {
-
-    //     const getVoices = () => {
-    //       voices = synth.getVoices();
-
-    //       // Loop through voices and create an option for each one
-    //       voices.forEach(voice => {
-    //         // Create option element
-    //         const option = document.createElement('option');
-    //         // Fill option with voice and language
-    //         option.textContent = voice.name + '(' + voice.lang + ')';
-
-    //         // Set needed option attributes
-    //         option.setAttribute('data-lang', voice.lang);
-    //         option.setAttribute('data-name', voice.name);
-    //         voiceSelect.appendChild(option);
-    //       });
-    //     };
-    // }
-
-    // getVoices(); // COMMENT AND UNCOMENT TO SEE CHANGES ON WEBPAGE
-
-
-
-
-    const [voices, setVoices] = useState([]);
 
     useEffect(() => {
         const synth = window.speechSynthesis;
@@ -177,26 +178,65 @@ export default function AppTranslatorVoice() {
     }, []);
 
 
-const dropDown = {
-    
-}
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log(text.text)
+        console.log(voices)
+        // console.log(voices[0].default) // default tells if languge is selected
+        // console.log(voices[0].name) // default tells if languge is selected
+        speak();
+        // textInput.blur();
+
+        // WORKS!!!
+        // let utteranceInit = new SpeechSynthesisUtterance(text.text)
+        // utteranceInit.lang = "fr-FR";
+        // speechSynthesis.speak(utteranceInit)
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setText(prevRec => {
+            // console.log(event.target.text)
+            // console.log(event.target)
+            return {
+                ...prevRec,
+                id: nanoid(),
+                [name]: value,
+            }
+        })
+    }
+
+    const handleSelect = (event) => {
+        // console.log(event.target.value)
+        const selectedOption = event.target.value
+        setLang(selectedOption)
+        console.log("LANG ++++++++ "+lang)
+    }
 
     return (
-        <form className='speak-wrapper'>
+        <form className='speak-wrapper'
+            onSubmit={handleSubmit}
+        >
             <textarea
                 className='textarea'
                 placeholder='text here'
+                name="text"
+                value={text.text}
+                onChange={handleChange}
             ></textarea>
 
-            <div class="form-group">
+            <div className="form-group">
                 <select
-                // id="voice-select"
+                    // id="voice-select"
+                    defaultValue={lang}
+                    onChange={handleSelect}
                 >
                     {voices.map((voice, index) => (
                         <option
-                        key={index}
-                        data-lang={voice.lang}
-                        data-name={voice.name}
+
+                            key={index}
+                            data-lang={voice.lang}
+                            data-name={voice.name}
                         >
                             {`${voice.name} (${voice.lang})`}
                         </option>
@@ -204,7 +244,7 @@ const dropDown = {
                 </select>
             </div>
 
-                    <button>speak</button>
+            <button>speak</button>
         </form>
     )
 }
