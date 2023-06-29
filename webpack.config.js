d5c35c8f391c080f8c446a119305df2e7c4baa1a
William S <will.step.205@gmail.com>
2023-06-28 21:56:59 -0700

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // resolve: {
+        fallback: { "crypto": require.resolve("crypto-browserify") },
+    //     fallback: { "path": require.resolve("path-browserify") },
+    //     fallback: { "os": require.resolve("os-browserify/browser") },
+    // }
+}
